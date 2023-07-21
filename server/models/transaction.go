8fbb1d0b45807b296fce5a11f4091f8b00196185
malfazakki <malfazakki@gmail.com>
2023-07-22 06:26:09 +0700

    modify transaction models

--- server/models/transaction.go
+++ server/models/transaction.go
@@ -4,7 +4,7 @@ type Transaction struct {
 	ID       int                `json:"id" gorm:"primary_key"`
 	UserID   int                `json:"user_id" form:"user_id" gorm:"type: int"`
 	User     UserTicketResponse `json:"user"`
-	TicketID int                `json:"ticket_id" form:"ticket_id" gorm:"type: int"`
+	TicketID int                `json:"ticket_id" form:"ticket_id" gorm:"type: int; onDelete:CASCADE; onUpdate:CASCADE; "`
 	Ticket   TicketResponse     `json:"ticket"`
 	Status   string             `json:"status" form:"status" gorm:"type: varchar(255)"`
 }
