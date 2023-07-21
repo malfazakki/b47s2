572fa6b59e231abe61f879527f63689a3609b624
malfazakki <malfazakki@gmail.com>
2023-07-21 21:33:04 +0700

    fix import privateroute

--- client/src/components/PrivateRoutes.jsx
+++ client/src/components/PrivateRoutes.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { UserContext } from "../context/userContext";
+import { UserContext } from "../context/UserContext";
 
 export function PrivateRouteLogin() {
   const [state] = useContext(UserContext);
